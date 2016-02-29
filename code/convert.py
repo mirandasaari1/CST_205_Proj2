8b6c68788e780f87a676bbaa97f6f3d7fdf1bc02
Nicholas R <nicholasirosas@student.hartnell.edu>
2016-02-29 09:49:30 -0800

    Added convert file and started main program file

---
+++ code/convert.py
@@ -0,0 +1,24 @@
+#This file contains all conversion functions we will need for this project
+
+#Summary: This function converts a decimal value to binary format
+#Precondition: Pass in a decimal value
+#Postcondition: Returns binary string of decimal value passed in
+def convertToBinary(dVal):
+	binNum = "{0:b}".format(dVal)
+	return binNum
+
+
+#Summary: This function converts a character to its correlated asci value
+#Preconditiion: Pass in a character
+#Postcondtion: Returns decimal number representing Asci value of that character
+def convertToAsci(ch)
+	asciVal = ord(ch)
+	return asciVal
+
+#Summary: This function converts a binary value to decimal format
+#Precondiition: Pass in binary value
+#Postcondtion: Returns decimal value representing the binary value passed in
+def convertToDecimal(bVal):
+	decVal = int(bVal, 2)
+	return decVal
+
---
+++ code/main.py
@@ -0,0 +1,26 @@
+from PIL import Image
+
+#Uncomment multiline comment below when there is a image in folder
+#type picture name in open() function below
+"""
+im = image.open(file name goes here)
+
+width, height = im.size
+
+rgb_im = im.convert('RGB')
+"""
+
+#below is a unfinished for loop that might be needed
+"""
+for x in range(0, width):
+	for y in range(0, height);
+		
+
+"""
+
+
+
+
+
+
+
